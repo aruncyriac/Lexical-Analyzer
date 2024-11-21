62faec4828cd0321773c27fe16410c18211f0f76
aruncyriac <aruncyriac01@gmail.com>
2024-11-21 18:37:49 +0530

    lexer21

--- lexer.c
+++ lexer.c
@@ -10,3 +10,44 @@ static const char* keywords[MAX_KEYWORDS] = {
 
 static const char* operators = "+-*/%=!<>|&";
 static const char* specialCharacters = ",;{}()[]";
+
+//to open the file
+void initializeLexer(const char* filename)
+{
+
+}
+//to get token
+Token getNextToken()
+{
+
+}
+//to categorize the token
+void categorizeToken(Token* token)
+{
+
+}
+//to check key word
+int isKeyword(const char* str)
+{
+
+}
+//to check operator
+int isOperator(const char* str)
+{
+
+}
+//to check spectialcharater
+int isSpecialCharacter(char ch)
+{
+
+}
+//to check constant
+int isConstant(const char* str)
+{
+
+}
+//to check identifer
+int isIdentifier(const char* str)
+{
+    
+}
--- lexer.h
+++ lexer.h
@@ -4,6 +4,7 @@
 #define MAX_KEYWORDS 20
 #define MAX_TOKEN_SIZE 100
 
+//to know the token
 typedef enum {
     KEYWORD,
     OPERATOR,
@@ -13,18 +14,26 @@ typedef enum {
     UNKNOWN
 } TokenType;
 
+//to store token
 typedef struct {
     char lexeme[MAX_TOKEN_SIZE];
     TokenType type;
 } Token;
-
+//to open the file
 void initializeLexer(const char* filename);
+//to get token
 Token getNextToken();
+//to categorize the token
 void categorizeToken(Token* token);
+//to check key word
 int isKeyword(const char* str);
+//to check operator
 int isOperator(const char* str);
+//to check spectialcharater
 int isSpecialCharacter(char ch);
+//to check constant
 int isConstant(const char* str);
+//to check identifer
 int isIdentifier(const char* str);
 
 #endif
